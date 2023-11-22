ca471535aa89e3ea3f316245df6ed42c301a4f15
OSTROVSKAY <OSTROVSKAY01989@gmail.com>
2023-11-22 15:13:29 +0300

    Finish : Module #4.1. Task #2. Boolean Conditions

--- src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
+++ src/main/java/school/mjc/stage0/conditions/task2/CoinFlip.java
@@ -3,5 +3,17 @@ package school.mjc.stage0.conditions.task2;
 public class CoinFlip {
     public void throwCoin(int from1UpTo1000) {
 
+        boolean check = from1UpTo1000 < 500;
+
+        if(check) {
+
+            System.out.println("Eagle");
+
+        } else {
+
+            System.out.println("Tail");
+
+        }
+
     }
 }
--- src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task2/GreatestNumberPrinter.java
@@ -2,5 +2,20 @@ package school.mjc.stage0.conditions.task2;
 
 public class GreatestNumberPrinter {
     public void printGreatest(int first, int second) {
+
+        boolean check = first > second;
+
+        if(check) {
+
+            System.out.println(first);
+
+        }
+
+        else {
+
+            System.out.println(second);
+
+        }
+
     }
 }
